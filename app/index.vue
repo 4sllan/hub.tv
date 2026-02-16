86e28260c063034b1eb361729fa546be2b118964
aslan.gama <aslan.gama@sixchains.com.br>
2026-02-15 22:57:14 -0300

    hub.tv

---
+++ app/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const {data} = await useLazyFetch('https://api.themoviedb.org/3/discover/movie', {
+  params:{
+    api_key: '',
+    language: 'pt-BR',
+  }
+})
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
